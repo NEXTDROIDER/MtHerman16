f9522301f9acc16775ce62f4e3ac5223b11706fb
K12S13 <Konstant.ber@gmail.com>
2026-06-06 16:35:32 +0300

    Auto-commit: file changed

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Spike : MonoBehaviour
+public class SpikeObstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Spike"))
+        {
+            RestartScene();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RestartScene()
     {
-        
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
     }
 }
