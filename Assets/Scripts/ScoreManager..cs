2ed7c3b11829ec0a0178b2d565e12d59a21be6e2
K12S13 <Konstant.ber@gmail.com>
2026-06-06 17:13:58 +0300

    Auto-commit: file changed

--- Assets/Scripts/ScoreManager..cs
+++ Assets/Scripts/ScoreManager..cs
@@ -11,12 +11,36 @@ public class ScoreManager : MonoBehaviour
 
     private void Awake()
     {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score : " + score;
+        }
+        else
+        {
+            Debug.LogError("ScoreManager is missing a reference to the score text UI.");
+        }
     }
 
     public void AddScore(int amount)
     {
         score += amount;
-        scoreText.text = "Score : " + score;
+        UpdateScoreText();
     }
 }
