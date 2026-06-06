28bc2d6b6c9a88320a5a175c0ae56654de9d00ec
K12S13 <Konstant.ber@gmail.com>
2026-06-06 16:36:03 +0300

    Auto-commit: file changed

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SpikeObstacle : MonoBehaviour
+public class Spike : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
