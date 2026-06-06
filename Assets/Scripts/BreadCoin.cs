df294ba380ac6f0d2d605e828feb06e8e989198c
K12S13 <Konstant.ber@gmail.com>
2026-06-06 17:19:51 +0300

    Auto-commit: file changed

--- Assets/Scripts/BreadCoin.cs
+++ Assets/Scripts/BreadCoin.cs
@@ -4,20 +4,25 @@ namespace MTHermanTF2
 {
     public class BreadCoin : MonoBehaviour
     {
+        private bool collected = false;
+
         private void OnTriggerEnter2D(Collider2D other)
         {
+            if (collected) return;
+
             if (other.CompareTag("Player"))
             {
+                collected = true;
+
                 Debug.Log("Bread collected!");
 
-                var scoreManager = ScoreManager.instance;
-                if (scoreManager != null)
+                if (ScoreManager.instance != null)
                 {
-                    scoreManager.AddScore(1);
+                    ScoreManager.instance.AddScore(1);
                 }
                 else
                 {
-                    Debug.LogError("ScoreManager instance not found. Add a ScoreManager object to the scene.");
+                    Debug.LogError("ScoreManager instance not found. Make sure ScoreManager exists in the scene.");
                 }
 
                 Destroy(gameObject);
